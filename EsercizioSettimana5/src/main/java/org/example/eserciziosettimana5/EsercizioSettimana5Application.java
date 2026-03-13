06506e80a0abfab86c0b2050b6ab81735518b8fd
emanueleumberto <info@emanueleumberto.it>
2026-03-13 09:30:14 +0100

    Traccia Esercizio Settimanale

---
+++ EsercizioSettimana5/src/main/java/org/example/eserciziosettimana5/EsercizioSettimana5Application.java
@@ -0,0 +1,13 @@
+package org.example.eserciziosettimana5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioSettimana5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsercizioSettimana5Application.class, args);
+    }
+
+}
---
+++ EsercizioSettimana5/src/test/java/org/example/eserciziosettimana5/EsercizioSettimana5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eserciziosettimana5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioSettimana5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
