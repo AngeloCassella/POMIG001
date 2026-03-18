3e9e9f16f94a3052d14cccec7b53fb6cd6c01a55
emanueleumberto <info@emanueleumberto.it>
2026-03-18 12:12:17 +0100

    Soluzione completa Esercizio settimanale MVC ed soluzione esercizio REST

---
+++ 18-SpringWebREST/src/main/java/org/example/_8springwebrest/repositories/UserRepo.java
@@ -0,0 +1,11 @@
+package org.example._8springwebrest.repositories;
+
+import org.example._8springwebrest.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    public User findByEmail(String email);
+}
+
